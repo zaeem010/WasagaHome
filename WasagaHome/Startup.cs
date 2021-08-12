e2f8592202cbc02694221e5d6d87a9ef27e66551
Zaeem khan <khanzaeem010@gmail.com>
2021-08-12 21:11:45 +0500

    Add project files.

---
+++ WasagaHome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WasagaHome.Startup))]
+namespace WasagaHome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
